0622557be9f9febf4a8832b2bc251daff6c5dca3
Prateek Chaurasia <prateek18801@gmail.com>
2023-03-23 16:34:15 +0530

    Add options import global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import Select, { SelectOption } from './components/Select';
 
+import './App.css';
+
 const options = [
-    { label: 'First', value: 1 },
-    { label: 'Second', value: 2 },
-    { label: 'Third', value: 3 },
-    { label: 'Fourth', value: 4 },
-    { label: 'Fifth', value: 5 },
-    { label: 'Sixth', value: 6 },
+    { label: 'One', value: 1 },
+    { label: 'Two', value: 2 },
+    { label: 'Three', value: 3 },
+    { label: 'Four', value: 4 },
+    { label: 'Five', value: 5 },
+    { label: 'Six', value: 6 },
+    { label: 'Seven', value: 7 },
+    { label: 'Eight', value: 8 },
+    { label: 'Nine', value: 9 },
+    { label: 'Ten', value: 10 }
 ]
 
 const App = () => {
@@ -16,11 +22,13 @@ const App = () => {
     const [multipleSelectValue, setMultipleSelectValue] = useState<SelectOption[]>([options[0]]);
 
     return (
-        <div className='App'>
+        <>
+            <h1>React Custom Select</h1>
+            <br />
             <Select options={options} value={singleSelectValue} onChange={option => setSingleSelectValue(option)} />
             <br />
             <Select multiple options={options} value={multipleSelectValue} onChange={option => setMultipleSelectValue(option)} />
-        </div>
+        </>
     );
 }
 
