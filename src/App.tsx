779ff2fcaf6d71ea04d307484a4052a6081e407a
Prateek Chaurasia <prateek18801@gmail.com>
2023-03-23 10:11:31 +0530

    Add sample options, value state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+import Select from './components/Select';
+
+const options = [
+    { label: 'First', value: 1 },
+    { label: 'Second', value: 2 },
+    { label: 'Third', value: 3 },
+    { label: 'Fourth', value: 4 },
+    { label: 'Fifth', value: 5 },
+    { label: 'Sixth', value: 6 },
+]
+
 const App = () => {
-    return <h1>Hello World</h1>;
+
+    const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+
+    return (
+        <div className='App'>
+            <Select options={options} value={value} onChange={option => setValue(option)} />
+        </div>
+    );
 }
 
 export default App;
